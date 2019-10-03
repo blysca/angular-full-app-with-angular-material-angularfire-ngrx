8ae57cb6414e02f1006ef75dd37493ff1fa38aac
Serhii Plyska <blysca@newtonideas.com>
2019-10-03 10:54:37 +0300

    *** fontawesome added

--- src/ng-refresher/src/app/app.module.ts
+++ src/ng-refresher/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './products/product/product.component';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [
@@ -15,10 +19,15 @@ import { ProductComponent } from './products/product/product.component';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab);
+  }
+}
--- src/ng-refresher/src/app/products/products.component.ts
+++ src/ng-refresher/src/app/products/products.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  faCoffee = faCoffee;
+  
   productName = 'book';
   isDisabled = true;
   products = ['a book', 'a tree'];
